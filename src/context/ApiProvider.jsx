8486484a5e9de869914215a5dbecfb7051a7dc50
Cristian Venegas <cristian.venegas@materoceano.com>
2022-09-11 22:32:06 -0300

    add : new NavBar

--- src/context/ApiProvider.jsx
+++ src/context/ApiProvider.jsx
@@ -2,24 +2,36 @@ import React, { createContext, useEffect, useState } from "react";
 
 export const ApiContext = createContext();
 
+
+
+/**
+ * GetData() es una función asíncrona que obtiene datos de la API, los ordena y establece el estado de
+ * los datos
+ */
 const ApiProvider = (props) => {
   const getData = async () => {
     const URL = "https://dragon-ball-super-api.herokuapp.com/api/characters";
     const response = await fetch(URL);
     const result = await response.json();
-    sortData(result, "name", true)
+    sortData(result, "name", true);
     setData(result);
-    // console.log(result)
   };
 
-  const sortData = (arr, key, asc=true) => {
-      return arr.sort((a, b) => {
-        let x = a[key];
-        let y = b[key];
-        if (asc) { return ((x < y) ? -1 : ((x > y) ? 1 : 0)); }
-        else { return ((x > y) ? -1 : ((x < y) ? 1 : 0)); }
-      });
-  }
+/**
+ * Ordena una matriz de objetos por una clave dada
+ * @returns una matriz ordenada.
+ */
+  const sortData = (arr, key, asc = true) => {
+    return arr.sort((a, b) => {
+      let x = a[key];
+      let y = b[key];
+      if (asc) {
+        return x < y ? -1 : x > y ? 1 : 0;
+      } else {
+        return x > y ? -1 : x < y ? 1 : 0;
+      }
+    });
+  };
 
   const filteredCharacter = () => {
     if (search === 0) {
@@ -27,26 +39,28 @@ const ApiProvider = (props) => {
     }
     const filtered = data.filter(
       (x) =>
-        x.name.toLocaleLowerCase().includes(search) ||
-        x.specie.toLocaleLowerCase().includes(search) ||
-        x.role.toLocaleLowerCase().includes(search) ||
-        x.universe.toLocaleLowerCase().includes(search) ||
-        x.status.toLocaleLowerCase().includes(search)
+        x.name.toLocaleLowerCase().includes(search.toLocaleLowerCase()) ||
+        x.specie.toLocaleLowerCase().includes(search.toLocaleLowerCase()) ||
+        x.role.toLocaleLowerCase().includes(search.toLocaleLowerCase()) ||
+        x.universe.toLocaleLowerCase().includes(search.toLocaleLowerCase()) ||
+        x.status.toLocaleLowerCase().includes(search.toLocaleLowerCase())
     );
     return filtered.slice(currentPage, currentPage + 8);
   };
 
   const nextPage = () => {
-    if(data.filter(
-      (x) =>
-        x.name.toLocaleLowerCase().includes(search) ||
-        x.specie.toLocaleLowerCase().includes(search) ||
-        x.role.toLocaleLowerCase().includes(search) ||
-        x.universe.toLocaleLowerCase().includes(search) ||
-        x.status.toLocaleLowerCase().includes(search)
-    ).length > currentPage + 8 ){
+    if (
+      data.filter(
+        (x) =>
+          x.name.toLocaleLowerCase().includes(search) ||
+          x.specie.toLocaleLowerCase().includes(search) ||
+          x.role.toLocaleLowerCase().includes(search) ||
+          x.universe.toLocaleLowerCase().includes(search) ||
+          x.status.toLocaleLowerCase().includes(search)
+      ).length >
+      currentPage + 8
+    ) {
       setCurrentPage(currentPage + 8);
-      
     }
   };
 
@@ -88,3 +102,4 @@ const ApiProvider = (props) => {
 };
 
 export default ApiProvider;
+
